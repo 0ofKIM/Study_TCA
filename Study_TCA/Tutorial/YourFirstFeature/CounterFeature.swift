59d639dd56d1064360eb98eb0fb7702c3e665b2b
uykim3 <uykim3@lotte.net>
2023-10-11 10:07:27 +0900

    section1 - What is a side effect?

--- Study_TCA/Tutorial/YourFirstFeature/CounterFeature.swift
+++ Study_TCA/Tutorial/YourFirstFeature/CounterFeature.swift
@@ -5,15 +5,19 @@
 //  Created by 0ofKim on 2023/10/10.
 //
 
+import Foundation
 import ComposableArchitecture
 
 struct CounterFeature: Reducer {
     struct State: Equatable {
         var count: Int = 0
+        var fact: String?
+        var isLoading: Bool = false
     }
 
     enum Action {
         case decrementButtonTapped
+        case factButtonTapped
         case incrementButtonTapped
     }
 
@@ -21,10 +25,26 @@ struct CounterFeature: Reducer {
         switch action {
         case .decrementButtonTapped:
             state.count -= 1
+            state.fact = nil
+            return .none
+
+        case .factButtonTapped:
+            state.fact = nil
+            state.isLoading = true
+
+            let (data, _) = try await URLSession.shared
+                .data(from: URL(string: "http://numbersapi.com/\(state.count)")!)
+            // 🛑 'async' call in a function that does not support concurrency
+            // 🛑 Errors thrown from here are not handled
+
+            state.fact = String(decoding: data, as: UTF8.self)
+            state.isLoading = false
+
             return .none
 
         case .incrementButtonTapped:
             state.count += 1
+            state.fact = nil
             return .none
         }
     }
